1197b2b287f61f12c741e76302a9a022ed70e79f
67160384 <67160384@go.buu.ac.th>
2025-11-04 12:04:49 +0700

    Update BubbleSort.py

--- BubbleSort.py
+++ BubbleSort.py
@@ -1,26 +1,26 @@
 class BubbleSorter:
-    def __init__(self, data):
-        self.data = data
-
-    def display(self):
-        print("Current data:", self.data)
+    def __init__(self,nums):
+        self.array = nums
 
     def sort(self):
-        n = len(self.data)
-        for i in range(n - 1):
-            for j in range(n - 1, i, -1):
-                if self.data[j] < self.data[j - 1]:
-                    self.data[j], self.data[j - 1] = self.data[j - 1], self.data[j]
-            print(f"After round {i + 1}:", self.data)  # show each round
+        n = len(self.array)
+        for i in range(0,n-1):
+            for j in range(0,n-i-1):
+                if self.array[j] > self.array[j+1]:
+                    self.array[j], self.array[j+1] = self.array[j+1], self.array[j]
+            print(f"After round {i + 1}:", self.array)
+
+    def display(self):
+        print("Current data:", self.array)
 
 if __name__ == "__main__":
     nums = [64, 34, 25, 12, 22, 11, 90]
     sorter = BubbleSorter(nums)
-
     print("Before sorting:")
     sorter.display()
+    print()
 
     sorter.sort()
-
-    print("\nAfter sorting:")
+    print()
+    print("After sorting:")
     sorter.display()
